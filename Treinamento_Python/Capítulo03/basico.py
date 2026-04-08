f187f7951e927a2084be44f0ab4663f5c4a2a653
Luis Pedro Costa <luispedroccosta@gmail.com>
2026-04-07 22:18:58 -0300

    Capítulo 03

---
+++ Treinamento_Python/Capítulo03/basico.py
@@ -0,0 +1,10 @@
+# Isso é uma lista
+estudantes_lst = [223 , 24, 33, 22, 21, 26, 123 , 25]
+
+print(type(estudantes_lst))
+print(estudantes_lst[1])
+
+# Isso é um dicionário
+estudantes_dict = {"Pedro":24, "Ana":22, "Ronaldo":26, "Janaina":25}
+
+print(type(estudantes_dict))

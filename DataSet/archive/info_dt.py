510555c2e7d883dffc4a3eb967a6fbda60b59aa7
Luis Pedro Costa <luispedroccosta@gmail.com>
2026-04-13 13:40:33 -0300

    Update

--- DataSet/archive/info_dt.py
+++ DataSet/archive/info_dt.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('C:\\Users\\luisp\\Documents\\python-learning\\DataSet\\archive\\Boston-house-price-data.csv')
-print(df.head())
+#print(df.head())
+
+print(df.info())

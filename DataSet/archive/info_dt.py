253e76d44fba3f1f1fc776901c74968a33233afd
Luis Pedro Costa <luispedroccosta@gmail.com>
2026-04-15 11:42:47 -0300

    Instalando as bibliotecas

--- DataSet/archive/info_dt.py
+++ DataSet/archive/info_dt.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('C:\\Users\\luisp\\Documents\\python-learning\\DataSet\\archive\\Boston-house-price-data.csv')
-#print(df.head())
+print("Matplotlib funcionando!")
 
-print(df.info())
+#df = pd.read_csv('C:\\Users\\luisp\\Documents\\python-learning\\DataSet\\archive\\Boston-house-price-data.csv')
+
+#print(df.info())

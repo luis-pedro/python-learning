0f7613d949588ad58dd50ff79d0069e3746e4876
Luis Pedro Costa <luispedroccosta@gmail.com>
2026-04-20 16:23:51 -0300

    Exercícios de Função - Capítulo 4

---
+++ Treinamento_Python/Capítulo04/Exercícios_Funções/exercício05.py
@@ -0,0 +1,10 @@
+n = 2
+
+soma = lambda n: n + 1
+
+print(soma(n))
+
+#OU
+
+soma2 = lambda arg1, arg2: arg1 + arg2
+print("A soma é: ", soma2(5,10))
---
+++ Treinamento_Python/Capítulo04/Exercícios_Funções/exercício08.py
@@ -0,0 +1,2 @@
+# Exercício 8 - Crie uma list comprehension que imprima o quadrado dos números de 1 a 10
+[x**2 for x in range(1,11)]
